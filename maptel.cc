8bd2297461451323120f30716504c617802084e0
Piotr Ulanowski <pioula111@gmail.com>
2021-10-19 18:45:19 +0200

    is_telephone_number, prefix do wypisywania, delete, create

--- maptel.cc
+++ maptel.cc
@@ -19,6 +19,8 @@ namespace {
     unordered_map<unsigned long, unordered_map<string, string>> maptel;
     unsigned long current_maptel = 0;
 
+    const string prefix = "maptel: ";
+
     string clone_string(const char *src) {
         string result = "";
         for (size_t k = 0; src[k]; k++) {
@@ -28,15 +30,40 @@ namespace {
         return result;
     }
 
+    bool is_telephone_number(char const *tel_num) {
+        size_t tel_length = strlen(tel_num);
+
+        if (tel_length == 0 || tel_length > TEL_NUM_MAX_LEN)
+            return false;
+
+        for (size_t i = 0; tel_num[i]; i++) {
+            if (!isdigit(tel_num[i]))
+                return false;
+        }
+
+        return true;
+    }
 }
 
 namespace jnp1 {
     unsigned long maptel_create(void) {
+        if (debug) {
+            cerr << prefix << "maptel_create()\n";
+        }
+
         unordered_map<string,string> new_maptel;
         maptel[current_maptel++] = new_maptel;
+
+        return current_maptel - 1;
     }
 
     void maptel_delete(unsigned long id) {
+        if (debug) {
+            assert(maptel.contains(id));
+
+            cerr << prefix << "maptel_delete(" << id << ")\n";
+        }
+
         maptel.erase(id);
     }
 
